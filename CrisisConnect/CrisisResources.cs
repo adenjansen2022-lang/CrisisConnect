1a3359816d02181bb43e60290bdfb83d79f899ce
Aden Jansen <adenjansen2022@gmail.com>
2026-08-09 17:51:35 +0200

    Add source files and gitignore

---
+++ CrisisConnect/CrisisResources.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrisisConnect
+{
+    abstract class CrisisResources
+    {
+        public string name {  get; set; }
+        public int disasterID { get; set; }
+        public string status { get; set; }//is the sqaud availabel or are they being deployed are they busy with maintenace
+        public string location {  get; set; }
+
+        public CrisisResources(string n, int dID, string s, string l)
+        {
+            name = n;
+            disasterID = dID;
+            status = s;
+            location = l;
+        }
+
+        public abstract string getTeamDetail();
+    }
+}
---
+++ CrisisConnect/MedicalTeam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrisisConnect
+{
+    internal class MedicalTeam:CrisisResources
+    {
+        public int NumDoctors {  get; set; }
+        public int NumAmbulances {  get; set; }
+
+        public MedicalTeam(string n, int dID, string s, string l, int ND, int NA) : base(n, dID, s, l)
+        {
+            NumAmbulances = NA;
+            NumDoctors = ND;
+        }
+
+        public override string getTeamDetail()
+        {
+            return $"Medical team ID: {disasterID} | Status: {status} | Location: {location} | Name: {name} | Doctors : {NumDoctors} | Ambulances: {NumAmbulances}";
+        }
+    }
+}
